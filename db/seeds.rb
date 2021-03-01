fdbe51ff4b86cda0ccff7434dc8f4f2e5de441a3
George <gchriscarter@gmail.com>
2021-03-01 17:47:17 -0500

    add 110 movies to db using ffaker

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,24 @@
 
 "require ffaker"
 actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
-actor.save
+#actor.save
 
-Actor.create(first_name: "Dan", last_name: "Ackroyd", known_for: "Ghostbusters")
+#Actor.create(first_name: "Dan", last_name: "Ackroyd", known_for: "Ghostbusters")
 
-Movie.create(title: "Star Wars", year: 1977, plot: "It is a period of civil wars in the galaxy. A brave alliance of underground freedom fighters has challenged the tyranny and oppression of the awesome GALACTIC EMPIRE.
+#Movie.create(title: "Star Wars", year: 1977, plot: "It is a period of civil wars in the galaxy. A brave alliance of underground freedom fighters has challenged the tyranny and oppression of the awesome GALACTIC EMPIRE.
 
-  Striking from a fortress hidden among the billion stars of the galaxy, rebel spaceships have won their first victory in a battle with the powerful Imperial Starfleet. The EMPIRE fears that another defeat could bring a thousand more solar systems into the rebellion, and Imperial control over the galaxy would be lost forever.
-  
-  To crush the rebellion once and for all, the EMPIRE is constructing a sinister new battle station. Powerful enough to destroy an entire planet, its completion spells certain doom for the champions of freedom. ")
+#   Striking from a fortress hidden among the billion stars of the galaxy, rebel spaceships have won their first victory in a battle with the powerful Imperial Starfleet. The EMPIRE fears that another defeat could bring a thousand more solar systems into the rebellion, and Imperial control over the galaxy would be lost forever.
 
-Movie.create(title: "National Treasure", year: 2004, plot: "A historian steals the Declaration of Independence on a treasure hunt")
+#   To crush the rebellion once and for all, the EMPIRE is constructing a sinister new battle station. Powerful enough to destroy an entire planet, its completion spells certain doom for the champions of freedom. ")
 
-Movie.create(title: "Raiders of the Lost Ark", year: 1981, plot: "A historian steals the Holy Grail on a treasure hunt")
+# Movie.create(title: "National Treasure", year: 2004, plot: "A historian steals the Declaration of Independence on a treasure hunt")
 
-Movie.create(title: "Toy Story", year: 1995, plot: "A kid's toys come to life when he's not around and go on adventures")
+# Movie.create(title: "Raiders of the Lost Ark", year: 1981, plot: "A historian steals the Holy Grail on a treasure hunt")
+
+# Movie.create(title: "Toy Story", year: 1995, plot: "A kid's toys come to life when he's not around and go on adventures")
+# 100.times do
+#   title = FFaker::Movie.title
+#   year = FFaker::Vehicle.year
+#   plot = FFaker::Book.description
+#   Movie.create(title: title, year: year, plot: plot)
+# end
