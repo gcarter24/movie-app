aa83ea5344b42f18751e6a9cc7a5b5e3ba1e5e7c
George <gchriscarter@gmail.com>
2021-03-13 18:04:54 -0500

    add genres, moviegenres, and many to many associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,6 @@ class Movie < ApplicationRecord
   validates :year, numericality: { greater_than: 1888 }, length: { minimum: 4, maximum: 4 }
   validates :plot, length: { minimum: 10 }
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
